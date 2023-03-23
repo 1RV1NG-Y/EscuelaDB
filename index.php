40fa32d82eeb3739546c57a6a0386aab3d94d5de
1RV1NG-Y <83391175+1RV1NG-Y@users.noreply.github.com>
2023-03-23 17:48:17 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html>
+    <head>
+        <link rel="shortcut icon" href="#" />
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="estilos.css">
+        <link rel="stylesheet" href="plugins/sweetalert2/sweetalert2.min.css">        
+        
+        <link rel="stylesheet" type="text/css" href="fuentes/iconic/css/material-design-iconic-font.min.css">
+        
+    </head>
+    
+    <body>
+     
+      <div class="container-login">
+        <div class="wrap-login">
+        <div class="users-form">
+        <form class="login-form validate-form" id="aula" action="">
+                <span class="login-form-title">Menu</span>
+                <div class="container-login-form-btn">
+                    <div class="wrap-login-form-btn">
+                        <div class="login-form-bgbtn"></div>
+                        <button type="submit" name="submit" class="login-form-btn">AULA</button>
+                    </div>
+                </div>
+            </form>
+            <form class="login-form validate-form" id="carrera" action="">
+                <div class="container-login-form-btn">
+                    <div class="wrap-login-form-btn">
+                        <div class="login-form-bgbtn"></div>
+                        <button type="submit" name="submit" class="login-form-btn">CARRERA</button>
+                    </div>
+                </div>
+            </form>
+            <form class="login-form validate-form" id="grupo" action="">
+                <div class="container-login-form-btn">
+                    <div class="wrap-login-form-btn">
+                        <div class="login-form-bgbtn"></div>
+                        <button type="submit" name="submit" class="login-form-btn">GRUPO</button>
+                    </div>
+                </div>
+            </form>
+            <form class="login-form validate-form" id="materia" action="">
+                <div class="container-login-form-btn">
+                    <div class="wrap-login-form-btn">
+                        <div class="login-form-bgbtn"></div>
+                        <button type="submit" name="submit" class="login-form-btn">MATERIA</button>
+                    </div>
+                </div>
+            </form>
+            <form class="login-form validate-form" id="filtros" action="">
+                <div class="container-login-form-btn">
+                    <div class="wrap-login-form-btn">
+                        <div class="login-form-bgbtn"></div>
+                        <button type="submit" name="submit" class="login-form-btn">FILTROS</button>
+                    </div>
+                </div>
+            </form>
+            <form class="login-form validate-form" id="groupby" action="">
+                <div class="container-login-form-btn">
+                    <div class="wrap-login-form-btn">
+                        <div class="login-form-bgbtn"></div>
+                        <button type="submit" name="submit" class="login-form-btn">GROUP BY</button>
+                    </div>
+                </div>
+            </form>
+            </div>
+        </div>
+    </div>     
+        
+        
+     <script src="jquery/jquery-3.3.1.min.js"></script>    
+     <script src="bootstrap/js/bootstrap.min.js"></script>    
+     <script src="popper/popper.min.js"></script>    
+        
+     <script src="plugins/sweetalert2/sweetalert2.all.min.js"></script>    
+     <script src="codigo.js"></script>    
+    </body>
+</html>
